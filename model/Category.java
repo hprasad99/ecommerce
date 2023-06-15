caca00b756e6012311937ea219520feb8bad4196
hprasad99 <himanshu.prasad@ril.com>
2023-06-15 16:04:27 +0530

    Initial commit

---
+++ model/Category.java
@@ -0,0 +1,34 @@
+package com.build.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    private @NotBlank String description;
+
+    private @NotBlank String imageUrl;
+}
---
+++ repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.build.ecommerce.repository;
+
+import com.build.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByCategoryName(String categoryName);
+}
